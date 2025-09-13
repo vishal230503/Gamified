596ecff63bf8e921b46256ff99784d460b9c126a
dev28hitesh <patilhitesho339@gmail.com>
2025-09-13 19:54:29 +0530

    Update GamiEdu.java

--- src/main/java/GamiEdu.java
+++ src/main/java/GamiEdu.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class GamiEdu {
 	public static void main(String args[]) {
 		int a;
+		int b;
 		Scanner sc = new Scanner(System.in);
 		a = sc.nextInt();
 		System.out.println("a: " + a);
+		System.out.println("b:"+b);
 	}
 }
