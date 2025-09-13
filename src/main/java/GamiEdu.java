d72c15b1f2db8b009555619efba72c40190489dc
Vishal Narkhede <vishalnarkhede2305@gmail.com>
2025-09-13 18:33:41 +0530

    java program

--- src/main/java/GamiEdu.java
+++ src/main/java/GamiEdu.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class GamiEdu {
 	public static void main(String args[]) {
-		
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		System.out.println("a: " + a);
 	}
 }
